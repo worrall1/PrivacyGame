ec9d2e58aabec36ed3ac2e1c90d664446542f727
ArmanFardin <fardinarman81@gmail.com>
2023-10-31 12:31:58 +0000

    settings update

--- Globals.gd
+++ Globals.gd
@@ -1,6 +1,6 @@
 extends Node
 
-<<<<<<< Updated upstream
+
 var money = 0
 var mps = 0
 var blueData = 0
@@ -8,16 +8,11 @@ var bdps = 0
 var redData = 0
 var rdps = 0
 var date = 0 #integer count of how many days have elapsed
-=======
-var money
-var blueData
-var redData
-var date
-var sfxVol
+var sfxVol = 0.5
 var musicVol = 0.5
 
 
->>>>>>> Stashed changes
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
--- SettingsVBox.gd
+++ SettingsVBox.gd
@@ -24,3 +24,17 @@ func _on_sfx_value_changed(value):
 func _on_music_value_changed(value):
 	Globals.musicVol = value / 100.0
 	pass # Replace with function body.
+
+
+func _on_exit_pressed():
+	get_tree().change_scene_to_file("res://TitleScreen.tscn")
+	pass # Replace with function body.
+
+
+func _on_fullscreen_toggled(button_pressed):
+	if button_pressed == true:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+	
+	pass # Replace with function body.
