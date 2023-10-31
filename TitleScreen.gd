bd26bfb312d1d09ad662c7eadfd45e6ca55a744b
duffieldg <g.duffield@lancaster.ac.uk>
2023-10-31 12:51:52 +0000

    FIXED buttons

--- TitleScreen.gd
+++ TitleScreen.gd
@@ -15,7 +15,7 @@ func _on_button_pressed():
 	get_tree().change_scene_to_file("res://HomeScreen.tscn")
 	pass # Replace with function body.
 
-	
-	
 
-
+func _on_button_2_pressed():
+	get_tree().change_scene_to_file("res://Settings.tscn")
+	pass # Replace with function body.
