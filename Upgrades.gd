86ac47e070f45696e0597720cb66a749a49858f4
duffieldg <g.duffield@lancaster.ac.uk>
2023-11-14 16:27:57 +0000

    Upgrades page

---
+++ Upgrades.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+var selected
+var currentSelection = null
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	selected = 0
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
--- VentureButton.gd
+++ VentureButton.gd
@@ -3,29 +3,39 @@ extends TextureButton
 var toggle = false
 var textura = load("res://Textures/selected_box.png")
 var default_texture: Texture
+var parent = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	default_texture = texture_normal
+	parent = get_parent()
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	
 	pass
 
 
 
 
 func _on_pressed():
-	if toggle == false:
-		texture_normal = textura
+	
 		
+	if toggle == false:
+		if parent.selected == 0:
+			
+			texture_normal = textura
+			parent.selected = 1
+			
 				
-		toggle = true
+			toggle = true
+			parent.selectedNode = self
 		
 	elif toggle == true:
 		texture_normal = default_texture
+		parent.selected=0
 		
 		
 		
