7498372bfcf410f116585bf1a3a89ecc00253421
worrall1 <thejonathanworrall@gmail.com>
2023-11-21 14:41:05 +0000

    Globals etc

--- BottomBar.gd
+++ BottomBar.gd
@@ -9,10 +9,10 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	get_node("HBoxContainer/VBoxContainer/Money").text = str(Globals.money)
-	get_node("HBoxContainer/VBoxContainer/MPS").text = str(Globals.mps)
+	get_node("HBoxContainer/VBoxContainer/MPS").text = str(Globals.mps) + "/s"
 	get_node("HBoxContainer/VBoxContainer2/BlueData").text = str(Globals.blueData)
-	get_node("HBoxContainer/VBoxContainer2/BDPS").text = str(Globals.bdps)
+	get_node("HBoxContainer/VBoxContainer2/BDPS").text = str(Globals.bdps) + "/s"
 	get_node("HBoxContainer/VBoxContainer3/RedData").text = str(Globals.redData)
-	get_node("HBoxContainer/VBoxContainer3/RDPS").text = str(Globals.rdps)
+	get_node("HBoxContainer/VBoxContainer3/RDPS").text = str(Globals.rdps) + "/s"
 	get_node("SecurityLevel").text = str(Globals.securityLevel)
 	pass
--- Globals.gd
+++ Globals.gd
@@ -15,13 +15,16 @@ var musicVol = 0.5
 var reputation = 100 #integer 1 - 100 value for reputation
 var securityFreq = 0
 var breachProb = 0.1
+var breachTemp = 0 #used when updating breach chance
+var breachThisQuarter = false
 var securityLevel = "Good"
 var quarter = 0 #how long the game has been going
 var time = 0 #how long since the start of this quarter
 var moneyLastQuarter = 0
 var blueDataLastQuarter = 0
 var repLastQuarter = 0
-
+var timeModifier = 0
+var reputationModifier = 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -30,8 +33,29 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if(timeModifier > 0):
+		money += mps * reputationModifier * timeModifier
+		moneyTotal += mps * reputationModifier * timeModifier
+		blueData += bdps * reputationModifier * timeModifier
+		blueDataTotal += bdps * reputationModifier * timeModifier
+		redData += rdps * reputationModifier * timeModifier
+		redDataTotal += rdps * reputationModifier * timeModifier
+		time += 1
+		breachTemp = redDataTotal
+		for i in range(securityFreq):
+			breachTemp = breachTemp / 2
+		breachProb = breachTemp
+		if (breachProb < 0.04):
+			securityLevel = "Good"
+		elif ((breachProb >= 0.04) && (breachProb < 0.2)):
+			securityLevel = "Threatened"
+		elif ((breachProb >= 0.2) && (breachProb < 0.5)):
+			securityLevel = "Poor"
+		else:
+			securityLevel = "Critical"
 	if(time > 2000):
 		quarter += 1
 		time = 0
+		
 		get_tree().change_scene_to_file("res://YearlyReport.tscn")
 	pass
