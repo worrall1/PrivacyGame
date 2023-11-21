6af242d7799b1b5a20786bf90d05694055354a69
ArmanFardin <fardinarman81@gmail.com>
2023-11-21 15:51:50 +0000

    Operations
    Can  now do PR activities that directly affect the reputation

--- Operations.gd
+++ Operations.gd
@@ -1,10 +1,18 @@
 extends CanvasLayer
 
-var hire_cost = 100 # Initial cost to hire a team member
+
+var hireCost = 100 # Initial cost to hire a team member
+var charityCost = 200
+var statementCost = 50
+var charityRep = 5
+var statementRep = 2
+var reputation_label
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	reputation_label = get_node("/root/CanvasLayer/BottomBar/ReputationLabel")
+	update_pr_buttons()
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -12,17 +20,18 @@ func _process(delta):
 	update_security_level()  # Update the security level
 	$SecLevel.text = str(Globals.securityLevel)
 	$BreachProb.text = str(Globals.breachProb)
-
+	$StatementCost.text = str(statementCost)
+	$CharityCost.text = str(charityCost)
 	# Disable the hire button if not enough money
-	$Hire.disabled = Globals.money < hire_cost
+	$Hire.disabled = Globals.money < hireCost
 
 	pass
 
 func _on_hire_pressed():
-	if Globals.money >= hire_cost:
+	if Globals.money >= hireCost:
 		Globals.securityFreq += 1
-		Globals.money -= hire_cost
-		hire_cost += 50 # Increase the cost for the next hire
+		Globals.money -= hireCost
+		hireCost += 50 # Increase the cost for the next hire
 
 		$SecurityLabel.text = str(Globals.securityFreq)
 	pass
@@ -41,3 +50,33 @@ func update_security_level():
 		Globals.securityLevel = "good"
 	else:
 		Globals.securityLevel = "excellent"
+
+func update_pr_buttons():
+	# Disables the PR buttons if there isn't enough money
+	$CharityButton.disabled = Globals.money < charityCost
+	$StatementButton.disabled = Globals.money < statementCost
+	
+
+
+func _on_charity_button_pressed():
+	if Globals.money >= charityCost:
+		Globals.money -= charityCost
+		Globals.reputation += charityRep
+		update_pr_buttons()
+		#reputation_label.text = str(Globals.reputation)
+
+func _on_statement_button_pressed():
+	if Globals.money >= statementCost:
+		Globals.money -= statementCost
+		Globals.reputation += statementRep
+		update_pr_buttons()
+		#reputation_label.text = str(Globals.reputation)
+		
+
+func update_pr_section():
+	pass
+
+
+
+
+
