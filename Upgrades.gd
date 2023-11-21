616df595c55f538c013076fda04e56b4810def7c
duffieldg <g.duffield@lancaster.ac.uk>
2023-11-21 14:51:50 +0000

    Upgrades

--- Upgrades.gd
+++ Upgrades.gd
@@ -4,12 +4,47 @@ var selected = 0
 var currentSelection = null
 
 
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	selected = 0
+	get_node("Button").hide()
+	get_node("Button2").hide()
+	get_node("Button3").hide()
+	get_node("Button4").hide()
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if selected ==1:
+		get_node("Label").hide()
+		get_node("Button").show()
+		get_node("Button2").show()
+		get_node("Button3").show()
+		get_node("Button4").show()
+		
+	elif selected == 0:
+		
+		get_node("Label").show()
+		get_node("Button").hide()
+		get_node("Button2").hide()
+		get_node("Button3").hide()
+		get_node("Button4").hide()
+		
+
+	
+	
+		
 	pass
+
+
+func _on_texture_button_pressed():
+	get_node("Label").hide()
+	get_node("Button").show()
+	get_node("Button2").show()
+	get_node("Button3").show()
+	get_node("Button4").show()
+	
+	pass # Replace with function body.
---
+++ VentureBarButton.gd
@@ -0,0 +1,20 @@
+extends Button
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+	get_node("Label").hide()
+	
+
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_pressed():
+	get_node("Label").show()
+	pass # Replace with function body.
