6299af8ddb1bc3e2ca5507ef81894233491041df
worrall1 <thejonathanworrall@gmail.com>
2023-11-21 15:27:20 +0000

    Actual Value Updates!

--- Globals.gd
+++ Globals.gd
@@ -1,13 +1,13 @@
 extends Node
 
 var money = 500
-var mps = 0
+var mps = 1
 var moneyTotal = 0
 var blueData = 0
-var bdps = 0
+var bdps = 1
 var blueDataTotal = 0
 var redData = 0
-var rdps = 0
+var rdps = 1
 var redDataTotal = 0
 var date = 0 #integer count of how many days have elapsed
 var sfxVol = 0.5
@@ -19,11 +19,12 @@ var breachTemp = 0 #used when updating breach chance
 var breachThisQuarter = false
 var securityLevel = "Good"
 var quarter = 0 #how long the game has been going
-var time = 0 #how long since the start of this quarter
+var time = 0
+var seconds = 0
 var moneyLastQuarter = 0
 var blueDataLastQuarter = 0
 var repLastQuarter = 0
-var timeModifier = 0
+var timeModifier = 1
 var reputationModifier = 1
 
 var upgradeArray = [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false]
@@ -36,29 +37,39 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if(timeModifier > 0):
-		money += mps * reputationModifier * timeModifier
-		moneyTotal += mps * reputationModifier * timeModifier
-		blueData += bdps * reputationModifier * timeModifier
-		blueDataTotal += bdps * reputationModifier * timeModifier
-		redData += rdps * reputationModifier * timeModifier
-		redDataTotal += rdps * reputationModifier * timeModifier
-		time += 1
-		breachTemp = redDataTotal
-		for i in range(securityFreq):
-			breachTemp = breachTemp / 2
-		breachProb = breachTemp
-		if (breachProb < 0.04):
-			securityLevel = "Good"
-		elif ((breachProb >= 0.04) && (breachProb < 0.2)):
-			securityLevel = "Threatened"
-		elif ((breachProb >= 0.2) && (breachProb < 0.5)):
-			securityLevel = "Poor"
-		else:
-			securityLevel = "Critical"
-	if(time > 2000):
-		quarter += 1
-		time = 0
-		
-		get_tree().change_scene_to_file("res://YearlyReport.tscn")
+	
+	time += delta
+
+	if (time >= 1):
+		time -= 1
+	
+		if(timeModifier > 0):
+			money += mps * reputationModifier * timeModifier
+			moneyTotal += mps * reputationModifier * timeModifier
+			blueData += bdps * reputationModifier * timeModifier
+			blueDataTotal += bdps * reputationModifier * timeModifier
+			redData += rdps * reputationModifier * timeModifier
+			redDataTotal += rdps * reputationModifier * timeModifier
+			seconds += 1
+			breachTemp = redDataTotal
+			for i in range(securityFreq):
+				breachTemp = breachTemp / 2
+			breachProb = breachTemp
+			if (breachProb < 0.04):
+				securityLevel = "Good"
+			elif ((breachProb >= 0.04) && (breachProb < 0.2)):
+				securityLevel = "Threatened"
+			elif ((breachProb >= 0.2) && (breachProb < 0.5)):
+				securityLevel = "Poor"
+			else:
+				securityLevel = "Critical"
+		if(seconds > 20):
+			quarter += 1
+			seconds = 0
+			var breachValue = randf()
+			if (breachValue < breachProb):
+				breachThisQuarter = true
+				reputation -= 40
+				redData = 0
+			get_tree().change_scene_to_file("res://YearlyReport.tscn")
 	pass
