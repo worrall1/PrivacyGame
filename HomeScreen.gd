83980d189ee090c0c01ebba3b7cb87724ab945b7
duffieldg <g.duffield@lancaster.ac.uk>
2023-12-06 19:23:28 +0000

    Updates

--- HomeScreen.gd
+++ HomeScreen.gd
@@ -3,11 +3,15 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$CPUParticles2D.amount= Globals.bdps * 10
+	$CPUParticles2D2.amount = Globals.mps
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	$Label.text = "£" + str(Globals.money)
+	
 	pass
 
 
